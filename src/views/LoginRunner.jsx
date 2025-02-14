377ad89db22dcfdf1f4c8131acda82f462c15c31
Chakhrisss1102 <saumax1102@gmail.com>
2025-02-14 12:13:29 +0700

    กำลังจะทำ Runofrunner

--- src/views/LoginRunner.jsx
+++ src/views/LoginRunner.jsx
@@ -1,42 +1,60 @@
-import React from 'react'
-import { Box, Typography, Avatar, TextField, Button } from '@mui/material'
-import run from './../assets/images/run.jpg'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Box, Typography, Avatar, TextField, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import run from './../assets/images/run.jpg';
 
 function LoginRunner() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLoginClick = async (e) => {
+    e.preventDefault();
+    if (!username || !password) {
+      alert('กรุณากรอกข้อมูลให้ครบ');
+      return;
+    }
+
+    try {
+
+      const response = await fetch(`http://localhost:3030/runner/${username}/${password}`, { method: 'GET' });
+      if(response.status === 200){
+        const responseData = await response.json();
+        localStorage.setItem('runner', JSON.stringify(responseData["data"]));
+
+        window.location.href = '/run/runofrunner';
+      }else if(response.status === 404){
+        alert('ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง');
+      }else{
+        alert('เข้าสู่ระบบไม่สำเร็จ');
+      }
+    } catch {
+      alert('แย่ๆๆ' + error);
+    }
+  };
+
   return (
-    <>
-      <Box sx={{ width : '100%' }}>
-        <Box sx={{ width: '60%', boxShadow: 2, mx: 'auto', mt: 10, py: 5 }}>
-          <Typography variant='h3' sx={{textAlign: 'center', color: '#011474'}}>
-            เข้าใช้งาน<br />Running Web Application
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ width: '60%', boxShadow: 2, mx: 'auto', mt: 10, py: 5 }}>
+        <Typography variant='h3' align='center' color='#011474'>
+          เข้าใช้งาน<br />Running Web Application
+        </Typography>
+        <Avatar alt='Running' src={run} variant='rounded' sx={{ mx: 'auto', mt: 3, width: 120, height: 120, boxShadow: 2 }} />
+        <Box sx={{ width: '60%', mx: 'auto', mt: 3 }}>
+          <TextField label='Username' fullWidth variant='outlined' sx={{ mb: 2 }} 
+          value={username} onChange={(e) => setUsername(e.target.value)} />
+          <TextField label='Password' type='password' fullWidth variant='outlined' sx={{ mb: 4 }} 
+          value={password} onChange={(e) => setPassword(e.target.value)} />
+          <Button variant='contained' fullWidth sx={{ py: 2, backgroundColor: '#011474' }} onClick={handleLoginClick}>
+            Login
+          </Button>
+          <Typography align='center' mt={4}>
+            ยังไม่มีบัญชีผู้ใช้?{' '}
+            <Link to='/runner/registerrunner' style={{ textDecoration: 'none', color: '#ff0000' }}>ลงทะเบียน</Link>
           </Typography>
-          <Avatar alt="Running" src={run} variant='rounded' sx={{mx: 'auto', mt:3, width: 120, height: 120, boxShadow: 2}} />
-          <Box sx={{width: '60%', mx:'auto', mt: 3}}>
-            <Typography>
-              ป้อนชื่อผู้ใช้
-            </Typography>
-            <TextField variant='outlined' fullWidth sx={{mt: 2, mb: 2}} label='Username'/>
-            <Typography>
-              ป้อนรหัสผ่าน
-            </Typography>
-            <TextField variant='outlined' fullWidth sx={{mt: 2, mb: 4}} type='password' label='Password'/>
-            <Button variant='contained' fullWidth sx={{pt: 2, pb: 2, backgroundColor: '#011474'}} >
-              Login
-            </Button>
-            <Typography sx={{mt: 4, textAlign: 'center'}}>
-              ยังไม่มีบัญชีผู้ใช้?
-              <Typography sx={{display: 'inline', ml: 1}}>
-                <Link to='/runner/registerrunner' style={{textDecoration:'none', color:'#ff0000'}}>
-                ลงทะเบียน
-                </Link>
-                </Typography>
-            </Typography>
-          </Box>
         </Box>
       </Box>
-    </>
-  )
+    </Box>
+  );
 }
 
-export default LoginRunner
+export default LoginRunner;
